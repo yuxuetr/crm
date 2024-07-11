0502bfe2ceb1d6f48e30160e810aaf986222f732
yuxuetr <116985868+54yeying@users.noreply.github.com>
2024-07-11 17:02:51 +0800

    feat: support use_stats proto3 and vscode config proto3 recognise path

---
+++ user-stat/build.rs
@@ -0,0 +1,15 @@
+use anyhow::Result;
+use std::fs;
+
+fn main() -> Result<()> {
+  fs::create_dir_all("src/pb")?;
+  let builder = tonic_build::configure();
+  builder.out_dir("src/pb").compile(
+    &[
+      "../protos/user-stats/messages.proto",
+      "../protos/user-stats/rpc.proto",
+    ],
+    &["../protos"],
+  )?;
+  Ok(())
+}
