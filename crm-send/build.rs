5a50e914bb142522e8a806020e1e98d9bec5dfde
yuxuetr <116985868+54yeying@users.noreply.github.com>
2024-07-12 15:26:00 +0800

    feat: support crm send service

---
+++ crm-send/build.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+use std::fs;
+
+fn main() -> Result<()> {
+  fs::create_dir_all("src/pb")?;
+
+  let builder = tonic_build::configure();
+  builder.out_dir("src/pb").compile(
+    &[
+      "../protos/notification/messages.proto",
+      "../protos/notification/rpc.proto",
+    ],
+    &["../protos"],
+  )?;
+  Ok(())
+}
